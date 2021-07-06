714bd4248cc44968baf045f2e333005b1514d446
Belmin Mahić <belmin.mahic@edu.fit.ba>
2021-07-06 10:37:27 +0200

    priprema mobitela i desktopa

---
+++ Kino/Kino.Mobile/Kino.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Kino.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Kino.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Kino/Kino.Mobile/Kino.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Kino.Mobile.Models;
+using Kino.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kino.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
